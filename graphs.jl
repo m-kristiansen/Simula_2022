4a4a9504f1b3dd5b11775c1a8b374167f314b146
mirok <miroslav.kuchta@gmail.com>
2022-06-22 16:58:44 +0200

    embedding graph

--- graphs.jl
+++ graphs.jl
@@ -33,7 +33,7 @@ return new_points', rot_matrix
 end
 
 
-function box_embed(x_values, y_values; padding=0.2, align::Bool=false, view::Bool=true)
+function get_bounding_box(x_values, y_values; padding=0.2, align::Bool=false, view::Bool=true)
 """ Takes the x and y values corresponding to the nodes of the graph and
     Generates a bounding box around the graph and a mesh on the surface of the box
     padding prevents the graph from intersecting the bounding box."""
@@ -58,18 +58,30 @@ function box_embed(x_values, y_values; padding=0.2, align::Bool=false, view::Boo
     b3 = (center_x+dx, center_y+dy)
     b4 = (center_x+dx, center_y-dy)
 
-
     box = [b1, b2, b3, b4]
     if align
         b = [[box[i][1] for i in 1:length(box)] [box[i][2] for i in 1:length(box)]] #reshape
         b = (inv(rot_matrix)*b')
         box = [(b[1, 1], b[2, 1]), (b[1,2], b[2,2]), (b[1,3], b[2,3]), (b[1, 4], b[2,4])] #reshape
     end
+    
+    return (b1, b2, b3, b4)
+end
 
-    bp1 = gmsh.model.geo.addPoint(box[1][1], box[1][2], 0)
-    bp2 = gmsh.model.geo.addPoint(box[2][1], box[2][2], 0)
-    bp3 = gmsh.model.geo.addPoint(box[3][1], box[3][2], 0)
-    bp4 = gmsh.model.geo.addPoint(box[4][1], box[4][2], 0)
+
+function box_embed(graph_nodes, graph_edges; padding=0.2, align::Bool=false, view::Bool=true)
+
+    gmsh.initialize()
+
+    gmsh.model.add("Graph_1")
+
+
+    b1, b2, b3, b4 = get_bounding_box(graph_nodes[:, 1], graph_nodes[:, 2], padding=padding, align=align, view=view)
+
+    bp1 = gmsh.model.geo.addPoint(b1[1], b1[2], 0)
+    bp2 = gmsh.model.geo.addPoint(b2[1], b2[2], 0)
+    bp3 = gmsh.model.geo.addPoint(b3[1], b3[2], 0)
+    bp4 = gmsh.model.geo.addPoint(b4[1], b4[2], 0)
 
     bl12 = gmsh.model.geo.addLine(bp1, bp2)
     bl23 = gmsh.model.geo.addLine(bp2, bp3)
@@ -80,51 +92,59 @@ function box_embed(x_values, y_values; padding=0.2, align::Bool=false, view::Boo
 
     boxsurface = gmsh.model.geo.addPlaneSurface([boxloop])
 
+    gmsh.model.geo.synchronize() # Sync CAD representation
+    
     gmsh.model.addPhysicalGroup(2, [boxsurface], 2) #(dim, tag, label)
     gmsh.model.setPhysicalName(2, 2, "My surface") #(dim, tag, label)
 
     gmsh.model.addPhysicalGroup(1, [bl12, bl23, bl34, bl41], 3) #(dim, tag, label)
     gmsh.model.setPhysicalName(1, 3, "sides") #(dim, tag, label)
 
+    gmsh.model.geo.synchronize() # Sync CAD representation
+    # At this point we are done with the tissure, now we need vasculature
+
+    graph_points = [gmsh.model.geo.addPoint(x..., 0) for x in eachrow(graph_nodes)]
+    
+    graph_lines = [gmsh.model.geo.addLine(graph_points[i], graph_points[j])
+                   for (i, j) in eachrow(graph_edges)]
+    print(graph_lines)
+    gmsh.model.geo.synchronize() # Sync CAD representation
+    
+    gmsh.model.geo.addPhysicalGroup(1, graph_lines, 1)
+    # gmsh.model.geo.setPhysicalName(1, 1, "Graph")
 
     gmsh.model.geo.synchronize() # Sync CAD representation
-    gmsh.model.mesh.generate(2)
 
+    gmsh.model.mesh.embed(1, graph_lines, 2, boxsurface)
+    
     if view
         gmsh.fltk.run()
     end
+
+    gmsh.model.mesh.generate(2)
 
     gmsh.finalize()
 end
 
 
-gmsh.initialize()
+X = [0.5, 0.5, 0.25, 0.75]
+Y = [0, 0.25, 0.5, 0.5]
 
-gmsh.model.add("Graph_1")
+graph_nodes = hcat(X, Y)
+graph_edges = [1 2;
+               2 3;
+	       2 4]
 
-X = rand(4, 1)
-Y = rand(4, 1)
-
-#Define nodes
-p1 = gmsh.model.geo.addPoint(X[1], Y[1], 0) #point 1, position
-p2 = gmsh.model.geo.addPoint(X[2], Y[2], 0) #point 2, position
-p3 = gmsh.model.geo.addPoint(X[3], Y[3], 0) #point 3, position
-p4 = gmsh.model.geo.addPoint(X[4], Y[4], 0) #point 4, position
-
-#Define lines connecting nodes
-l12 = gmsh.model.geo.addLine(p1, p2) #line 1, from p1 to p4
-l23 = gmsh.model.geo.addLine(p2, p3) #line 2, from p2 to p4
-l34 = gmsh.model.geo.addLine(p3, p4) #line 3, from p4 to p3
-
-gmsh.model.geo.addPhysicalGroup(1, [l12, l23, l34], 1)
-#gmsh.model.geo.setPhysicalName(1, 1, "Graph")
+box_embed(graph_nodes, graph_edges, padding=0.01, align = false, view = true)
+	       
+# #Define nodes
 
 
-box_embed(X, Y, padding=0.01, align = true, view = true)
+# 
 
 
 
-"""
-if distance from node_i < padding
-    node_i_x = max_node_x
-"""
+# """
+# if distance from node_i < padding
+#     node_i_x = max_node_x
+# """
