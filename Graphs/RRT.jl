6b53fc503b026cd39eff867c903afa165bb239f5
Martin Kristiansen <m.kristiansen@me.com>
2022-07-25 09:42:26 +0200

    Normalized, and included visual stress test

--- Graphs/RRT.jl
+++ Graphs/RRT.jl
@@ -1,8 +1,9 @@
 using Distributions
+using Random
 using Plots
-using LinearAlgebra
+using LinearAlgebra:norm
 
-function RRT(num_points, connectivity, starting_point, seed)
+function RRT(num_points, connectivity, seed; starting_point=[0.0,0.0])
     Random.seed!(seed)
     N = num_points
     x0 = starting_point[1]
@@ -24,8 +25,45 @@ return points
 end
 
 
-function connect_RRT(points, max_norm)
+function line_intersection(p1, p2, p3, p4)
+    """
+    return true and point of intersection if lines intersect
+    """
+
+    x1, y1 = p1 # a point on the first line
+    x2, y2 = p2 # another point on the first line
+    x3, y3 = p3 # a point on the second line
+    x4, y4 = p4 # another point on the second line
+
+    #check paralell:
+    if (x1-x2)*(y3-y4) == (y1-y2)*(x3-x4)
+        return false
+    end
+
+    px = ((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4))
+    py = ((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4))
+
+    #scalar value desciding point of intersection on line 1
+    t = ((x1-x3)*(y3-y4)-(y1-y3)*(x3-x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4)) #scalar value desciding point of intersection on line 1
+
+    #scalar value desciding point of intersection on line 2
+    u = ((x1-x3)*(y1-y2)-(y1-y3)*(x1-x2))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4))
+
+    s = [t, u]
+
+    if all(>=(0), s) & all(<=(1), s)
+        return true, [px,py]
+    end
+
+return false, []
+end
+
+
+
+function connect_RRT(points, ;max_norm::Float64 = 1.0)
     connections = [] #store connections [(point1, point1), ..]
+    POI = [] #if intersect, append here
+    nodes = points
     N = size(points[:, 1])[1]
     for i in 2:N #skip starting point
         iter = 0
@@ -38,17 +76,38 @@ function connect_RRT(points, max_norm)
             end
         end
     end
-
-return points, hcat(collect.(connections)...)'
+    #check if intersect
+    for i in 1:length(connections)
+        for j in 1:length(connections)
+            if i<j
+                if (connections[i][1] ∉ connections[j]) & (connections[i][2] ∉ connections[j])
+                    bool, p = line_intersection(points[connections[i][1], :], points[connections[i][2], :],
+                        points[connections[j][1], :], points[connections[j][2], :])
+                    if bool
+                        push!(POI, p)
+                        println("intersection at: ", POI)
+                    end
+                end
+            end
+        end
+    end
+    nodes = nodes/maximum(nodes) #normalize
+return nodes, hcat(collect.(connections)...)', POI
 end
 
-if PROGRAM_FILE == basename(@__FILE__)
-    points  = RRT(10, 0.9, [1.0, 1.0])
-    nodes, lines = connect_RRT(points, 1.0)
+false && begin
+    #visual stress test
+    for seed in rand(1:1000, 100)
+        println("seed: ", seed)
+        global points = RRT(30, 1, seed)
+        nodes, lines, POI = connect_RRT(points)
 
-    p = scatter(nodes[:, 1], nodes[:, 2])
-    for (i, j) in lines
-        plot!([nodes[i, 1], nodes[j, 1]], [nodes[i, 2], nodes[j, 2]], color = :blue)
+        scatter(nodes[:, 1], nodes[:, 2])
+
+        for (i,j) in eachrow(lines)
+            plot!([nodes[i, 1], nodes[j, 1]],[[nodes[i, 2], nodes[j, 2]]], color = :blue, show = true, legend = false,
+            xlim = (0,1.01), ylim = (0,1.01))
+        end
+        sleep(1.0) #time delay between seeds
     end
-    savefig(p, "RRT.png")
 end
